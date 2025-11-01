63415bcdf5d6d52a713cf94f6a177f98da971e66
Ula <ursz.kam@gmail.com>
2025-11-01 22:06:15 +0100

    init commit

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_name" {
+    description = ""
+    type        = string
+}
+
+variable "subnet_name" {
+    description = ""
+    type        = string
+}
+
+variable "cidr_block" {
+    description = ""
+    type        = string
+}
+
+variable "region" {
+    description = ""
+    type        = string
+}
