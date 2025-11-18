0ca01f53e6b207814d86f3e49338e96d9aec2974
MsCzarna <kajakarolinka@gmail.com>
2025-11-18 21:07:09 +0100

    Update variables.tf

--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -1,34 +1,34 @@
 variable "project_id" {
-  description = ""
+  description = "Name of the project"
   type        = string
 }
 
 variable "region" {
-  description = ""
+  description = "Name of the region"
   type        = string
 }
 
 variable "vm_name" {
-  description = ""
+  description = "Name of the VM instance"
   type        = string
 }
 
 variable "machine_type" {
-  description = ""
+  description = "Name of the machine type"
   type        = string
 }
 
 variable "subnet_self_link" {
-  description = ""
+  description = "Name of the subnet self link"
   type        = string
 }
 
 variable "service_account_email" {
-  description = ""
+  description = "Name of the service acount email"
   type        = string
 }
 
 variable "env" {
-  description = ""
+  description = "name of the env"
   type        = string
 }
