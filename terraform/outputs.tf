3e91e3629a9891d80e283a72b35fd3e29842bbf1
MsCzarna <kajakarolinka@gmail.com>
2025-11-18 21:08:36 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "vm_internal_ip" {
-  description = ""
+  description = "name of internal IP"
   value       = module.vm.vm_internal_ip
 }
 
 output "vm_service_account_email" {
-  description = ""
+  description = "name of VM service account"
   value       = module.iam.vm_service_account_email
 }
 
 output "monitoring_service_account_email" {
-  description = ""
+  description = "Name of Monitoring service account"
   value       = module.iam.monitoring_service_account_email
 }
