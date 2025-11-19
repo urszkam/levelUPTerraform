aa06eea02af007f46c35e53dff315f6035476719
Ula <66919420+urszkam@users.noreply.github.com>
2025-11-19 22:09:01 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
-  description = "Name of the project ID"
+  description = "GCP Project ID"
   type        = string
 }
 
 variable "project_name" {
-  description = "Name of the project"
+  description = "Common project prefix for service names"
   type        = string
 }
 
 variable "region" {
-  description = "name fo the region"
+  description = "The GCP region where the resources are created"
   type        = string
 }
 
 variable "env" {
-  description = "name of the env"
+  description = "Name of the environment (e.g., dev)"
   type        = string
 }
 
 variable "cidr_block" {
-  description = "name of the cidr_block"
+  description = "CIDR block used for network resources"
   type        = string
 }
 
 variable "notification_email" {
-  description = "name of the notification"
+  description = "Email address used for sending notifications"
   type        = string
 }
