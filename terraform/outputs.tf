8552f0cf185a600fda8932383dc1a3e3abb2bb8d
Ula <66919420+urszkam@users.noreply.github.com>
2025-11-19 22:13:32 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "vm_internal_ip" {
-  description = "name of internal IP"
+  description = "Internal IP address of the vm"
   value       = module.vm.vm_internal_ip
 }
 
 output "vm_service_account_email" {
-  description = "name of VM service account"
+  description = "Email address of the VM's service account"
   value       = module.iam.vm_service_account_email
 }
 
 output "monitoring_service_account_email" {
-  description = "Name of Monitoring service account"
+  description = "Email address of the monitoring service account"
   value       = module.iam.monitoring_service_account_email
 }
