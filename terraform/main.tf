68ec88ebe863ee2fc2171b0df5e2a4ec77b61e60
Ula <66919420+urszkam@users.noreply.github.com>
2025-11-20 02:33:43 +0100

    Revert "Increase VM size for rollback test"

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ module "vm" {
   project_id            = var.project_id
   region                = var.region
   vm_name               = "${var.project_name}-${var.env}-vm"
-  machine_type          = "e2-medium"
+  machine_type          = "e2-small"
   subnet_self_link      = module.network.subnet_self_link
   service_account_email = module.iam.vm_service_account_email
   env                   = var.env
